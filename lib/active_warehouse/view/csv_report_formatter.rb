1a046002bb22c160c11e02efde0d6d82b6afe835
RobertT <siniypin@gmail.com>
2013-11-22 21:07:28 +0400

    fixed csv headers generating code

--- lib/active_warehouse/view/csv_report_formatter.rb
+++ lib/active_warehouse/view/csv_report_formatter.rb
@@ -6,21 +6,21 @@ class CsvReportFormatter
       column_dimension = table_view.column_dimension
       row_dimension = table_view.row_dimension
 
-      csv << row_dimension.hierarchy_level_label
+      columns = []
+	  columns << row_dimension.hierarchy_level_label
       column_dimension.values.each do |col_dim_value|
-        csv << col_dim_value
+        columns << col_dim_value
       end
-
+	  csv << columns
+	  
       table_view.data_rows.each do |data_row|
         result_row = []
 
         result_row << data_row.dimension_value
-
         unless data_row.cells.collect { |c| c.raw_value }.reduce(:+) == 0
           data_row.cells.each_with_index do |cell, index| # aggregated facts
             result_row << cell.value
           end
-
           csv << result_row
         end
       end
